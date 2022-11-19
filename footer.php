8e5dc4edbd4a7a0bd82e10d6b5cb2315d900c2b1
iargotes <iargotes@gmail.com>
2022-11-18 21:00:41 -0500

    Primer commit de pagina web

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer id="footer" class="dark">
+  <div id="copyrights" style="background-color:#111;">
+    <div class="container-fluid">
+
+      Copyrights &copy; Canvas 2021 | All Rights Reserved
+
+    </div>
+  </div>
+  <?php wp_footer();?>
+</footer>
---
+++ header.php
@@ -0,0 +1,109 @@
+<?php?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Montserrat:400,700|Merriweather&display=swap" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+
+	<!-- Media Agency Demo Specific Stylesheet -->
+	<link rel="stylesheet" href="demos/media-agency/media-agency.css" type="text/css" />
+	<!-- / -->
+
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="one-page/css/et-line.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="demos/media-agency/fonts.css" type="text/css" />
+
+	<link rel="stylesheet" href="css/custom.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>Media Agency | Canvas</title>
+  <?php wp_head();?>
+</head>
+
+<body data-loader="14">
+
+	<div class="side-cover-wrapper vh-100 d-none d-lg-block">
+		<div class="fslider h-100" data-speed="3000" data-pause="5000" data-animation="fade" data-arrows="false" data-pagi="false" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; background-color: #333;">
+			<div class="flexslider" style="height: 100% !important;">
+				<div class="slider-wrap" style="height: inherit !important;">
+					<div class="slide" style="background: url('demos/media-agency/images/1.jpg') center right; background-size: cover; height: 100% !important;"></div>
+					<div class="slide" style="background: url('demos/media-agency/images/2.jpg') center right; background-size: cover; height: 100% !important;"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="border-bottom-0 no-sticky">
+			<div id="header-wrap">
+				<div class="container-fluid">
+					<div class="header-row">
+
+						<!-- Logo
+						============================================= -->
+						<div id="logo" class="mr-lg-5">
+							<a href="index.html" class="standard-logo" data-dark-logo="demos/media-agency/images/logo-media-dark.png"><img src="demos/media-agency/images/logo-media.png" alt="Canvas Logo"></a>
+							<a href="index.html" class="retina-logo" data-dark-logo="demos/media-agency/images/logo-media-dark@2x.png"><img src="demos/media-agency/images/logo-media@2x.png" alt="Canvas Logo"></a>
+						</div><!-- #logo end -->
+
+						<div class="header-misc">
+
+							<div class="d-flex">
+								<a href="#" class="social-icon si-small si-borderless si-facebook">
+									<i class="icon-facebook"></i>
+									<i class="icon-facebook"></i>
+								</a>
+
+								<a href="#" class="social-icon si-small si-borderless si-twitter">
+									<i class="icon-twitter"></i>
+									<i class="icon-twitter"></i>
+								</a>
+
+								<a href="#" class="social-icon si-small si-borderless si-github">
+									<i class="icon-github"></i>
+									<i class="icon-github"></i>
+								</a>
+							</div>
+
+						</div>
+
+						<div id="primary-menu-trigger">
+							<svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
+						</div>
+
+						<!-- Primary Navigation
+						============================================= -->
+						<nav class="primary-menu mr-lg-auto">
+
+							<ul class="menu-container one-page-menu" data-easing="easeInOutExpo" data-speed="1250" data-offset="60">
+								<li class="menu-item"><a class="menu-link" href="#" data-href="#section-about" data-offset="200"><div>About</div></a></li>
+								<li class="menu-item"><a class="menu-link" href="#" data-href="#section-services"><div>Services</div></a></li>
+								<li class="menu-item"><a class="menu-link" href="#" data-href="#section-portfolio"><div>Portfolio</div></a></li>
+								<li class="menu-item"><a class="menu-link" href="#" data-href="#section-contact"><div>Contact</div></a></li>
+							</ul>
+
+						</nav><!-- #primary-menu end -->
+
+					</div>
+				</div>
+			</div>
+			<div class="header-wrap-clone"></div>
+		</header><!-- #header end -->
